423216a53766f489148cfad5af52b7f3972ccd33
Catherine Cherubin <cat.cherubin@gmail.com>
2019-05-22 12:04:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -65,6 +65,7 @@ def monopoly_with_fourth_tier
 			:pieces => 4,
 			:names => {
 				:reading_railroad => {
+					"mortgage_value" => "$100"
 
 				},
 				:pennsylvania_railroad => {
@@ -76,7 +77,6 @@ def monopoly_with_fourth_tier
 				:shortline => {
 
 				}
-
 			},
 			:rent_in_dollars => {
 				:one_piece_owned => 25,
