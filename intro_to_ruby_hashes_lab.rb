be1ad129f289776c3042a9a5a8e8a8f5cf43c62a
Catherine Cherubin <cat.cherubin@gmail.com>
2019-05-22 11:55:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,8 +15,7 @@ def monopoly
 	}
 
 end
-monopoly hash that we started building in the #monopoly method #monopoly
-_with_second_tier sets the first key of the :railroads hash to a symbol, :pieces, whose value is the integer 4
+
 def monopoly_with_second_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
