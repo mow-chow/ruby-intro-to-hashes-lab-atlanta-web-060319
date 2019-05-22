bc4667fdb903017963724200ad8b21799ec16e50
Catherine Cherubin <cat.cherubin@gmail.com>
2019-05-22 11:59:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -44,8 +44,6 @@ def monopoly_with_third_tier
 		}
 	}
 end
-third level of the monopoly hash that we continued building in the #monopoly_with_second_tie
-r method #monopoly_with_third_tier sets the 2nd key of the :rent_in_dollars hash to a symbol, :two_pieces_owned, whose value is the integer 50
 def monopoly_with_fourth_tier
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
 
